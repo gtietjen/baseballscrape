f126ab8abd4e1557376b4a7663bba1b0e99321f7
Gary Tietjen <garytietjen@Garys-MacBook-Pro-2.local>
2017-05-13 21:00:24 -0400

    Updated from Anto's personal repo: https://github.com/FelicianoAnthony/Python_projects/blob/master/web-scraping/scrape_salary_tables.py

--- index.py
+++ index.py
@@ -1,19 +1,41 @@
+#!/usr/bin/env python
 import requests
 from bs4 import BeautifulSoup
-page = requests.get('http://www.baseball-reference.com/players/a/alberma01.shtml').text
+import pandas as pd
+
+def scrape_salary_table(player_url):
+    
+    page = requests.get(player_url).text
 
 
-table_code = page[page.find('<table class="sortable stats_table" id="br-salaries"'):]
-soup = BeautifulSoup(table_code, 'lxml')
+    table_code = page[page.find('<table class="sortable stats_table" id="br-salaries"'):]
+    soup = BeautifulSoup(table_code, 'lxml')
 
-table_body  = soup.find('tbody')
-body_len= len(table_body)
+    table_body  = soup.find('tbody')
+    
+    #this block is for salary
+    salary_lst = []
+    for i in table_body.findAll('tr'):
+        for j in i.findAll('td'):
+            if j['data-stat'] == 'salary':
+                sal = j.get_text()
+                salary_lst.append(sal)
+                
+    #this block is for years              
+    years = table_body.findAll('th')                          
+    years_lst = []
+    for i in years:
+        ls = i.get_text()
+        years_lst.append(ls)
+    del years_lst[-1]
+        
+    df = pd.DataFrame(years_lst)
+    df['salary'] = salary_lst
+    df1 = df.reset_index()
+    df2 = df1.rename(columns={0: 'years'})
+    df3 = df2.set_index(keys = ['years'])
+    return df3
 
-for i in range(body_len):
-    the_row = []
-    table_row=table_body.findAll('tr')
-    for tr in table_row:
-        value  = tr.get_text()
-        the_row.append(value)
+url = input('Enter a path bitch')
 
-        #Anto's Code
+scrape_salary_table(url)
